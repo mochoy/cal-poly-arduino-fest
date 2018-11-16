4da8edb2d14910b16adda6e5e45772c85169f3fa
etnom <montgomery.okdaily@gmail.com>
2018-11-15 22:18:35 -0800

    install rc-slider

--- web-client/src/Components/App/App.js
+++ web-client/src/Components/App/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1 id="title">Web-Controlled Nerf Blaster</h1>
-        
+
       </div>
     );
   }
