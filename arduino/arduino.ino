6293d85496716d4b5e6ebb42aa42e1e7f461926a
etnom <montgomery.okdaily@gmail.com>
2018-11-15 23:28:05 -0800

    turn on onboard LED upon serial read

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,11 +1,17 @@
 
+#define LED_PIN 13
+
 
 void setup() {
 	Serial.begin(9600);
+
+	pinMode(LED_PIN, HIGH);
 }
 
 void loop() {
 	if(Serial.available() > 0) {
-		Serial.println((String)Serial.read());
-	} 
+		digitalWrite(LED_PIN, HIGH);
+	} else {
+		digitalWrite(LED_PIN, LOW);
+	}
 }
