4c88a10fd0988c06b85425365088d5d50b3708f6
etnom <montgomery.okdaily@gmail.com>
2018-11-15 22:24:35 -0800

    render sliders and corresponding titles

--- web-client/src/Components/App/App.js
+++ web-client/src/Components/App/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
+import Slider from 'rc-slider';
 
 import './App.css';
+
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
 
 class App extends Component {
   constructor(props) {
@@ -14,7 +17,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1 id="title">Web-Controlled Nerf Blaster</h1>
-
+        <div id="sliders-container">
+          <div className="slider-wrapper">
+            <h3 className="slider-title">Power</h3>
+            <Slider min={0} max={100} defaultValue={80}/>
+          </div>
+          <div className="slider-wrapper">
+            <h3 className="slider-title">Rate of Fire</h3>
+            <Slider min={0} max={100} defaultValue={80}/>
+          </div>
+        </div>
+        
       </div>
     );
   }
